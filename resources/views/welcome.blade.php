f54fdc32be42fdf839f6cc0ef1cb5626877dfe27
xF3rd <fernando.bordin.correa@gmail.com>
2022-10-12 20:57:47 -0300

    Comecei a estilizar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,25 @@
 
 @section('content')
 
-@foreach($profs as $prof)
-    <p>{{ $prof->name }}</p>
-@endforeach
+<div id="search-container" class="col-md-12">
+    <h1>Busque um professor</h1>
+    <form action="">
+        <input type="text" id="search" name="form-control" placeholder="Buscar...">
+    </form>
+</div>
+<div id="prof-container" class="col-md-12">
+    <h2>Professores</h2>
+    <p>Professores cadastrados</p>
+    <div id="cards-conteiner" class="row">
+        @foreach($profs as $prof)
+        <div class="card col-md-3">
+            <img src="/img/guarana.png" alt="{{ $prof->name }}">
+            <div class="card-body">
+                <h5 class="card-title">{{ $prof->name }}</h5>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 
 @endsection
